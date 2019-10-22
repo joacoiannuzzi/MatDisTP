a04cae417191dcf102f775acab10734383cfdede
joaquin iannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2019-10-22 10:57:30 -0300

    Merge branch 'master' of /Users/joacoiannuzzi/projects/MatDis TP with conflicts.

--- src/TransportNet.kt
+++ src/TransportNet.kt
@@ -1,10 +1,14 @@
-class TransportNet {
+class TransportNet<Element>(capacity: Int = 10) {
 
-    var n = 0
-    var alpha = 0
+    class Vertex<Element>(var key: Element)
+    class Edge(var maxFlux: Int, var currentFlux: Int) {
+        fun print() {
+            println("maxFlux = $maxFlux, currentFlux = $currentFlux")
+        }
+    }
 
-    var list = listOf(0, 8, 6, 0)
-
-
-
+    private var n = 0
+    private var alpha = 0
+    private val vertexes = arrayOfNulls<Vertex<Element>>(capacity)
+    private var matrix = Array(capacity) { arrayOfNulls<Edge>(capacity)}
 }

9b151190a355c751738e34ac33a9c39c409f732b
joaquin iannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2019-10-23 11:19:12 -0300

    fixed tests

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -1,27 +1,27 @@
 import main.FlowNetwork
 import org.junit.Test
+import kotlin.test.assertEquals
 
-fun main() {
+class Test {
 
-    val t = FlowNetwork<String>(6)
-    t.addVertex("a")
-    t.addVertex("b")
-    t.addVertex("d")
-    t.addVertex("g")
-    t.addVertex("h")
-    t.addVertex("z")
+    @Test
+    fun `The flow of this network should be 11`() {
+        val t = FlowNetwork<String>(6)
+        t.addVertex("a")
+        t.addVertex("b")
+        t.addVertex("d")
+        t.addVertex("g")
+        t.addVertex("h")
+        t.addVertex("z")
 
-    t["a", "b"] = 5; t["a", "g"] = 7
-    t["b", "d"] = 4; t["b", "h"] = 6
-    t["d", "z"] = 5
-    t["g", "h"] = 5
-    t["h", "d"] = 2; t["h", "z"] = 6
+        t["a", "b"] = 5; t["a", "g"] = 7
+        t["b", "d"] = 4; t["b", "h"] = 6
+        t["d", "z"] = 5
+        t["g", "h"] = 5
+        t["h", "d"] = 2; t["h", "z"] = 6
 
-    println(t.fordFulkerson("a", "z"))
-}
+        val flow = t.fordFulkerson("a", "z")
 
-
-@Test
-fun test() {
-
+        assertEquals(11, flow)
+    }
 }
